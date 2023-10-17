70ea2e1a0dda7b089c6a2b4211288443c780cdfd
Vivek <vivekparde2411@gmail.com>
2023-10-17 14:29:58 +0530

    remove unused code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,7 @@
 import { Toaster } from "react-hot-toast";
 import Routes from "./routes/Routes";
-import { useEffect, useState } from "react";
-import axiosInstance from "./helpers/axiosInstance";
 
 const App = () => {
-  const [userData, setUserData] = useState([]);
-
-  useEffect(() => {
-    (async () => {
-      const { data } = await axiosInstance.get("/user/profile");
-      setUserData(data.userData);
-    })();
-  }, []);
-
   return (
     <>
       <Toaster
